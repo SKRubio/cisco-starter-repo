5136b7d9a8eb850ac4d92838c32685a907d630b6
saulorubio <61097342+SKRubio@users.noreply.github.com>
2023-02-15 17:28:42 -0800

    remove unnecessary files and create components
    all unnecessary files were removed from the project and the main components for the dashboard were created Banner.js and DataPoint.js .

---
+++ sextant/src/Banner.js
@@ -0,0 +1,11 @@
+import './index.css';
+
+function Banner({text}){
+    return (
+        <div>
+            <h1 className="banner">{text}</h1>
+        </div>
+    );
+}
+
+export default Banner;
---
+++ sextant/src/DataPoint.js
@@ -0,0 +1,8 @@
+function DataPoint({title, data}){
+    return (<div>
+        <h1>{title} {data}</h1>
+    </div>
+    );
+}
+
+export default DataPoint;
